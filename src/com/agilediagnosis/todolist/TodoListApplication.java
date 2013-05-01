7f510af2e79a8b2891df1a17f0d4cc308681d588
Greg Woloschyn <greg.woloschyn@gmail.com>
2013-05-01 13:04:50 -0400

    testincg again

--- src/com/agilediagnosis/todolist/TodoListApplication.java
+++ src/com/agilediagnosis/todolist/TodoListApplication.java
@@ -1,5 +1,9 @@
 package com.agilediagnosis.todolist;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import com.agilediagnosis.todolist.logging.ALog;
 
 import android.app.Application;
@@ -20,9 +24,21 @@ public class TodoListApplication extends Application {
 	public static final String TODO_LIST_WEB_APP_URL = "http://www.gregwoloschyn.com";
 	public static final int HTTP_TIMEOUT_CONNECTION_MILLIS = 10000;
 	public static final int HTTP_TIMEOUT_SOCKET_MILLIS = 15000;
+
+	private static final int MAX_FILE_SIZE = 500000;
+	private static final String gitCommitIdFileName = "git-commit-id.txt";
+	public static String gitCommitId = "";
 	
 	public void onCreate() {
 		super.onCreate();
 		ALog.v(TAG, "");
+		
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open(gitCommitIdFileName)), MAX_FILE_SIZE);
+			gitCommitId = reader.readLine();
+			ALog.v(TAG, "read gitCommitId = " + gitCommitId);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
